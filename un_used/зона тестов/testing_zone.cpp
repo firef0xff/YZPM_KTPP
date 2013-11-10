30c0a17dd14c8da563c0222df003982b1db1a216
Chistov Maxim <Chistov.m.a@gmail.com>
2013-11-10 20:51:13 +0300

    project_save

--- un_used/зона тестов/testing_zone.cpp
+++ un_used/зона тестов/testing_zone.cpp
@@ -8,20 +8,16 @@
 USEFORM("..\маркетинг\продукция\Agrements_select.cpp", Agreements_select);
 USEFORM("..\маркетинг\продукция\products.cpp", prod); /* TFrame: File Type */
 USEFORM("..\маркетинг\продукция\Select_params_prod.cpp", Sel_params_prod);
-USEFORM("..\отчетность\params.cpp", parametr);
-USEFORM("..\отчетность\pp_start_params.cpp", PP_params);
-USEFORM("..\отчетность\trb_params.cpp", TrbOptions);
-USEFORM("..\отчетность\kol_det.cpp", koldet);
-USEFORM("..\отчетность\komplect.cpp", komplekt);
-USEFORM("..\интерфейс запусков\manufacture.cpp", ManufactureControl); /* TFrame: File Type */
+USEFORM("..\маркетинг\продукция\Add_zak.cpp", Zak_prop);
 USEFORM("test.cpp", Form1);
+USEFORM("..\маркетинг\contractors.cpp", Contragents);
 USEFORM("..\маркетинг\договора\agreements_main.cpp", Agreements); /* TFrame: File Type */
 USEFORM("..\маркетинг\договора\Select_params_agr.cpp", Sel_params_agr);
-USEFORM("..\маркетинг\продукция\Add_zak.cpp", Zak_prop);
-USEFORM("..\интерфейс запусков\ZakazCreator.cpp", ZakazWnd);
-USEFORM("..\интерфейс запусков\ZapCreator.cpp", ZapuskWnd);
-USEFORM("..\маркетинг\contractors.cpp", Contragents);
-USEFORM("..\интерфейс запусков\IzdAppender.cpp", IzdAppenderWnd);
+USEFORM("..\..\reports\kol_det.cpp", koldet);
+USEFORM("..\..\reports\komplect.cpp", komplekt);
+USEFORM("..\..\reports\params.cpp", parametr);
+USEFORM("..\..\reports\pp_start_params.cpp", PP_params);
+USEFORM("..\..\reports\trb_params.cpp", TrbOptions);
 //---------------------------------------------------------------------------
 WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
