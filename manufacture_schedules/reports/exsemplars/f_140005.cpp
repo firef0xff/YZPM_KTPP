6fb005cea90c6c86a0a92d458e8b0e147b1751db
Max Chistov <chistov.m.a@gmail.com>
2014-01-02 14:04:44 +0400

    Добавление формы 140005 к формируемым документам при запуске

---
+++ manufacture_schedules/reports/exsemplars/f_140005.cpp
@@ -0,0 +1,84 @@
+#include <reports/exsemplars/f_140005.h>
+#include <xl_operations.h>
+#include "functions.h"
+
+namespace rep
+{
+
+F140005::F140005 (int set): rep::Report("Форма 140005: План производства деталей",set),
+    DB(0),path(""),use_listing(false),lists_by_file(0),object(""),element(""),type(""),template_path(""),
+    cur_lists(0),templ("manufacture_forms.xlt")
+{
+    params[REPORT_PATH];
+    params[REPORT_LIST_COUNT] = "10";
+    params[REPORT_USE_LISTING] = REP_TRUE;
+    params["Цех"];
+    params["Участок"];
+}
+F140005::~F140005()
+{}
+
+F140005::F140005(const F140005 &r):rep::Report(r),
+    DB(0),path(""),use_listing(false),lists_by_file(0),object(""),element(""),type(""),template_path(""),
+    cur_lists(0),templ("manufacture_forms.xlt")
+{
+
+}
+
+void F140005::Build(void)
+{
+    cur_lists = 0;
+    ParseParams();
+    LoadSettings();
+    BuildReport();
+}
+boost::shared_ptr<rep::Report> F140005::SelfCopy (void) const
+{
+    return boost::shared_ptr<rep::Report>(new F140005(*this));
+}
+
+void F140005::ParseParams(void)
+{
+    DB = ReportList::Instance().DbConnection();
+    if (!DB)
+        throw std::runtime_error("Отсутствует соединение с базой данных");
+
+    //парсинг входных параметров отчета
+    path = params[REPORT_PATH];
+    use_listing = params[REPORT_USE_LISTING] == REP_TRUE;
+
+    std::string s_tmp = params[REPORT_LIST_COUNT];
+    std::stringstream s;
+    s << s_tmp;
+    s >> lists_by_file;
+
+    if (!lists_by_file && !s_tmp.empty())
+        throw std::runtime_error("Количество станиц на файл должно быть числом");
+
+    //переменные указывающие на цель отчета
+    object = params[REPORT_OBJECT_ID];
+    type   = params[REPORT_OBJECT_TYPE];
+    element = params[REPORT_ELEMENT_ID];
+
+    if (object.empty())
+        throw std::runtime_error("Не указан объект для построения отчета");
+    if (type.empty())
+        throw std::runtime_error("Не указан тип объекта для отчета");
+}
+void F140005::LoadSettings()
+{
+    //считать переменные из бд
+    TADOQuery *rez = DB->SendSQL("select value from administration.settings where property='template'");
+    if (rez)
+    {
+        template_path = rez->FieldByName("Value")->Value.operator AnsiString().Trim().c_str();
+        delete rez;
+        rez = 0;
+    }
+    else
+    {
+        throw std::runtime_error("Не задан путь к шаблонам");
+    }
+}
+
+}
---
+++ manufacture_schedules/reports/exsemplars/f_140005.h
@@ -0,0 +1,45 @@
+#ifndef F_140005_H
+#define F_140005_H
+#include <reports/report.h>
+
+namespace rep
+{
+
+class F140005 : public Report
+{
+public:
+    F140005(int set);
+    ~F140005();
+
+    F140005(const F140005 &r);
+
+    void Build(void);
+    boost::shared_ptr<rep::Report> SelfCopy (void) const;
+
+private:
+
+    //соединение в бд
+    cSQL *DB;
+    //настройки размещения отчета
+    std::string path;
+    bool use_listing;
+    size_t lists_by_file;
+    //объект построения отчета
+    std::string object;
+    std::string element;
+    std::string type;
+
+    //шаблоны
+    std::string template_path;//путь к файлу шаблона
+    std::string templ;
+
+    //глобальные переменные
+    size_t cur_lists;
+
+    void ParseParams    (void);
+    void LoadSettings   (void);
+    void BuildReport    (void){}
+};
+
+}
+#endif // F_140005_H
