662d741537294eebc0c80e2a5866cb5ac7b837a5
Max Chistov <chistov.m.a@gmail.com>
2014-05-12 22:38:26 +0400

    sql fix

--- manufacture_schedules/manufacture.cpp
+++ manufacture_schedules/manufacture.cpp
@@ -212,7 +212,7 @@ void TManufactureControl::LoadZapusk(String zapusk, String zakaz, String det)
     if (!Show_Closed->Checked)
 		where += " and b.close_date is null ";
     else
-        where += " and b.close_date not null ";
+        where += " and b.close_date is not null ";
 
     if (zapusk.Length())
     {
@@ -270,7 +270,7 @@ void TManufactureControl::LoadZakaz (String zap_id, String zakaz, String det)
     if (!Show_Closed->Checked)
         where += " and a.close_date is null ";
     else
-        where += " and a.close_date not null ";
+        where += " and a.close_date is not null ";
     if (zap_id.Length())
     {
         where += " and c.zap_id = '"+zap_id+"' ";
